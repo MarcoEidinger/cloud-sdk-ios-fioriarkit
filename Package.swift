543035c072b614421f9f9528538034a8a697e5b1
Marco Eidinger <eidingermarco@gmail.com>
2022-03-24 11:29:26 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-import PackageDescription
+import PackageDescription // NEW
 
 let package = Package(
     name: "FioriAR",
